46751d0166d49fe3a8bf9659dfcaf3c91aa96813
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-10-19 12:52:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    console.log(a+b);
+}
+
+add(5,5)
+
+function multiply(a, b) {
+    console.log(a*b);
+}
+
+multiply(5,5)
+
+function minus(a, b) {
+    console.log(a-b);
+}
+
+minus(5,5)
+
+function divide(a, b, c) {
+    console.log(a/b+c);
+}
+
+divide(5,5,9)
